7a674148e7acb76efe5528f0de297a83924efe40
jangle <shubhamjangale0000@gmail.com>
2020-11-19 15:09:41 +0530

    Added : Added the evenet listener for validating date and name

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -3,3 +3,23 @@ const output = document.querySelector('.salary-output');
 salary.addEventListener('input', function(){
   output.textContent = salary.value;
 });
+
+const text = document.querySelector('#name');
+const textError = document.querySelector('.name-error');
+text.oninput = function(){
+  let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+    if(nameRegex.test(text.value) || text.value == "")
+      textError.textContent = "";
+    else
+      textError.textContent = "Name is Incorrect";
+};
+
+const date = document.querySelector("#year");
+const dateError = document.querySelector('.date-error');
+date.oninput = function(){
+  let dateLimit = Date.now();
+  if(new Date(date.value) < dateLimit)
+    dateError.textContent = "";
+  else
+    dateError.textContent = "Put a valid Date";
+};
