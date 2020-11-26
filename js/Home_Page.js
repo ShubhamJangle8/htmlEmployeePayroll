8e4b97142e038b6393fafad6faffb6003a20cd27
jangle <shubhamjangale0000@gmail.com>
2020-11-26 14:01:04 +0530

    Added : Added functionality to get details from Json object and refactored the images directories

--- js/Home_Page.js
+++ js/Home_Page.js
@@ -1,64 +1,41 @@
+
+let employeePayrollList;
 window.addEventListener('DOMContentLoaded', (event) => {
-  createInnerHtml();
+    employeePayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector('.emp-count').textContent = employeePayrollList.length;
+    createInnerHtml();
 });
 
 const createInnerHtml = () => {
-  const headerHtml = "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>StartDate</th><th>Actions</th></tr>";
-  let innerHtml = `${headerHtml}`;
-  let employeePayrollList = createEmployeePayrollJSON();
-  for (const empPayrollData of employeePayrollList) {
-      innerHtml = `${innerHtml}
-      <tr> 
-          <td><img class = "profile" src = "${empPayrollData._profilePic}" alt = ""></td>
-          <td>${empPayrollData._name}</td>
-          <td>${empPayrollData._gender}</td>
-          <td>${getDeptHtml(empPayrollData._department)}</td>
-          <td>${empPayrollData._salary}</td>
-          <td>${empPayrollData._startDate}</td>
-          <td>
-              <img id = "${empPayrollData._id}" src = "../assets/icons/delete-black-18dp.svg" onclick = "remove(this)" alt = "delete">
-              <img id = "${empPayrollData._id}" src = "../assets/icons/create-black-18dp.svg" onclick = "update(this)" alt = "edit">
-          </td>
-      </tr>
-      `;
-  }
-  document.querySelector('#table-display').innerHTML = innerHtml;
+    const headerHtml = "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>StartDate</th><th>Actions</th></tr>";
+    let innerHtml = `${headerHtml}`;
+    if (employeePayrollList.length == 0) return;
+    for (const empPayrollData of employeePayrollList) {
+        innerHtml = `${innerHtml}
+        <tr> 
+            <td><img class = "profile" src = "${empPayrollData._profilePic}" alt = ""></td>
+            <td>${empPayrollData._name}</td>
+            <td>${empPayrollData._gender}</td>
+            <td>${getDeptHtml(empPayrollData._department)}</td>
+            <td>${empPayrollData._salary}</td>
+            <td>${empPayrollData._startDate}</td>
+            <td>
+                <img name = "${empPayrollData._id}" src = "../assets/icons/delete-black-18dp.svg" onclick = "remove(this)" alt = "delete">
+                <img name = "${empPayrollData._id}" src = "../assets/icons/create-black-18dp.svg" onclick = "update(this)" alt = "edit">
+            </td>
+        </tr>
+        `;
+    }
+    document.querySelector('#table-display').innerHTML = innerHtml;
 }
 const getDeptHtml = (deptList) => {
-  let deptHtml = '';
-  for (const dept of deptList) {
-      deptHtml = `${deptHtml}<div class = "dept-label">${dept}</div>`;
-  }
-  return deptHtml;
+    let deptHtml = ``;
+    for (const dept of deptList) {
+        deptHtml = `${deptHtml}<div class = "dept-label">${dept}</div>`;
+    }
+    return deptHtml;
 }
 
-createEmployeePayrollJSON = () => {
-  let employeePayrollList = [
-      {
-          _name: 'Maria',
-          _gender: 'Female',
-          _department: [
-              'HR',
-              'Finance'
-          ],
-          _salary: '400000',
-          _startDate: '1 October 2020',
-          _note: '',
-          _id: new Date().getTime(),
-          _profilePic: '../assets/profile-images/Ellipse 1.png'
-      },
-      {
-          _name: 'Rafel',
-          _gender: 'Male',
-          _department: [
-              'Engineering',
-          ],
-          _salary: '400000',
-          _startDate: '1 November 2020',
-          _note: '',
-          _id: new Date().getTime(),
-          _profilePic: '../assets/profile-images/Ellipse -5.png'
-      }
-  ];
-  return employeePayrollList;
+const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ? JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
 }
